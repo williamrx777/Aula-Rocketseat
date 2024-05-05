919b7f76f2465583c9b5527826135bfdb33c6f13
William Rodrigues <williamr723@gmail.com>
2024-05-05 16:32:56 -0300

    adicionando mais um perfil para o light mode

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
     const switchButton = document.querySelector('#switch button');
-    
+    const avatarImg = document.querySelector('#avatar');
 
     switchButton.addEventListener('click', function () {
         document.body.classList.toggle('light');
         if (document.body.classList.contains('light')) {
             switchButton.style.right = 'auto';
             switchButton.style.left = '0';
-            
+            avatarImg.src = 'avatar-light.png';
             switchButton.style.backgroundImage = 'url(sun-stars.svg)';
         } else {
             switchButton.style.left = 'auto';
             switchButton.style.right = '0';
-            
+            avatarImg.src = 'avatar.png';
             switchButton.style.backgroundImage = 'url(moon-stars.svg)';
         }
     });
