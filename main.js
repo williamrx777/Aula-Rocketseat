59335feab977d6b70e75d5862bdbfa29ef8e4ded
William Rodrigues <williamr723@gmail.com>
2024-05-08 20:24:45 -0300

    efeito no botão de dark e light mode

--- main.js
+++ main.js
@@ -5,13 +5,13 @@ document.addEventListener('DOMContentLoaded', function () {
     switchButton.addEventListener('click', function () {
         document.body.classList.toggle('light');
         if (document.body.classList.contains('light')) {
-            switchButton.style.right = 'auto';
-            switchButton.style.left = '0';
+            switchButton.style.left = 'auto';
+            switchButton.style.right = '0';
             avatarImg.src = 'avatar-light.png';
             switchButton.style.backgroundImage = 'url(sun-stars.svg)';
         } else {
-            switchButton.style.left = 'auto';
-            switchButton.style.right = '0';
+            switchButton.style.right = 'auto';
+            switchButton.style.left = '0';
             avatarImg.src = 'avatar.png';
             switchButton.style.backgroundImage = 'url(moon-stars.svg)';
         }
